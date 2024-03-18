c35968ca926b5848b42ef80dcd65eb36255950b2
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-03-18 22:04:35 +0000

    commit

---
+++ arkfrontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Container>
+          {/* 2:59:20 */}
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ arkfrontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+function Header() {
+  return (
+    <div>
+        <Button  variant="warning">Header</Button>
+    </div>
+  )
+}
+
+export default Header
